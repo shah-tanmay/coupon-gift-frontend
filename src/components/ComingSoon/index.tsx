3916deea32207a4b753f721bc28113255721eabe
Tanmay <shahhtanmay@gmail.com>
2023-02-02 22:51:21 +0530

    safari changes

--- src/components/ComingSoon/index.tsx
+++ src/components/ComingSoon/index.tsx
@@ -29,7 +29,7 @@ const ComingSoon = () => {
   return (
     <>
       <div className="h-screen w-screen grid place-items-center text-center">
-        <div className="absolute">
+        <div className="absolute w-screen h-screen">
           <div className="md:w-[500px] w-[300px] md:h-[500px] h-[500px]">
             <Lottie options={rocketDefaultOptions} />
           </div>
